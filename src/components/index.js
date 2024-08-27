132c377c99d743ca45d1303e75d3023bfd6339c4
Sandesh Ayyar <ayyarsandesh@gmail.com>
2024-08-28 00:30:40 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -6,7 +6,7 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import Signup from "./SignUp";
+import SignUp from "./SignUp";
 import Login from "./Login";
 import Button from "./Button";
 import PostForm from "./post-form/PostForm";
